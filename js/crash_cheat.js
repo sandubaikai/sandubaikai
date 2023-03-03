c25a8ea57982d62c2425ebf36ef01276b97ac0b4
sandubaikai <sandubaikai@163.com>
2023-03-03 15:33:37 +0800

    Site updated: 2023-03-03 15:33:36

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,16 +1,1 @@
-var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/joke.ico");
-         document.title = '要进来喝杯咖啡吗？';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '遗落之地~';
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?($('[rel="icon"]').attr("href","/joke.ico"),document.title="要进来喝杯咖啡吗？",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title="遗落之地~",titleTime=setTimeout((function(){document.title=OriginTitle}),2e3))}));
